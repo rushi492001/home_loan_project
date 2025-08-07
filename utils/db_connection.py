e4d67ac96afc3f82e68397a2d54a77c80fb3ad90
Rushikesh <rushichavan492001@gmail.com>
2025-08-07 20:59:42 +0530

    Update db_connection.py

--- utils/db_connection.py
+++ utils/db_connection.py
@@ -4,16 +4,16 @@ import streamlit as st
 def get_connection():
     try:
         conn = pyodbc.connect(
-            'Driver={ODBC Driver 18 for SQL Server};'
-            'Server=tcp:rushi.database.windows.net,1433;'
-            'Database=sourcehomeloan;'
-            'Uid=RushiServer;'
-            'Pwd=Rutuja492001;'
-            'Encrypt=yes;'
-            'TrustServerCertificate=yes;'
-            'Connection Timeout=30;'
-        )
+        'DRIVER={ODBC Driver 18 for SQL Server};'
+        'SERVER=tcp:rushi.database.windows.net,1433;'
+        'DATABASE=sourcehomeloan;'
+        'UID=Rushi492001;'
+        'PWD=Rutuja492001;'
+        'Encrypt=yes;'
+        'TrustServerCertificate=yes;'
+        'Connection Timeout=30;')
         return conn
     except pyodbc.Error as err:
         st.error(f"❌ SQL Server Connection Error: {err}")
         return None
+
